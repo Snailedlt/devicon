c58fa34800fe225d5763366fbcffa08f93a4f359
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2022-10-02 06:39:57 +0200

    sdfsdgs

--- .github/scripts/in_develop_labeler.py
+++ .github/scripts/in_develop_labeler.py
@@ -9,7 +9,13 @@ def main():
 		print("test develop")
 		print(args.body.split("\n"))
 		# find the issue closing line
-		issue_line = [line for line in args.body.split("\n") if line.startswith("**This PR closes")][0]
+		issue_line = ""
+		for line in args.body.splitlines():
+			print(line)
+			if line.startswith("**This PR closes")[0]:
+				issue_line = line
+				break
+			raise IndexError()
 
 		print("Issue Line is " + issue_line)
 		issue_pattern = re.compile(r"\d+")
